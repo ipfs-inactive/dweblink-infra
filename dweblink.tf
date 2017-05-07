11ba732d7d4b33b084d42b09bcd666cfdece2840
Lars Gierth <larsg@systemli.org>
2017-05-07 07:42:06 +0200

    Be a lot more declarative

--- dweblink.tf
+++ dweblink.tf
@@ -3,115 +3,66 @@ resource "vultr_ssh_key" "lars-desktop" {
   public_key = "${file("lars-desktop.pub")}"
 }
 
-variable "gw_count" {
-  default = 1
+variable "datacenters" {
+  default = ["ams","fra","cdg"]
 }
-
-variable "lb_count" {
-  default = 1
-}
-
 variable "regions" {
-  default = [7, 9]
+  default = {
+    ams = "eu",
+    fra = "eu",
+    cdg = "eu"
+  }
+}
+variable "datacenter_to_vultr_region" {
+  default = {
+    ams = 7,
+    fra = 9,
+    cdg = 24
+  }
+}
+variable "inventory" {
+  default = {
+    "0" = { hostname = "gw0.ams", datacenter = "ams", role = "gw", vultr_name = "ipfs-ams-gw0", vultr_plan = 203 },
+    "1" = { hostname = "gw1.ams", datacenter = "ams", role = "gw", vultr_name = "ipfs-ams-gw1", vultr_plan = 203 },
+    "2" = { hostname = "gw0.fra", datacenter = "fra", role = "gw", vultr_name = "ipfs-fra-gw0", vultr_plan = 203 },
+    "3" = { hostname = "gw1.fra", datacenter = "fra", role = "gw", vultr_name = "ipfs-fra-gw1", vultr_plan = 203 },
+    "4" = { hostname = "lb0.ams", datacenter = "ams", role = "lb", vultr_name = "ipfs-ams-lb0", vultr_plan = 203 },
+    "5" = { hostname = "lb0.fra", datacenter = "fra", role = "lb", vultr_name = "ipfs-fra-lb0", vultr_plan = 203 }
+  }
 }
 
-variable "region_names" {
-  default = ["ams", "fra", "cdg"]
-}
-
-resource "vultr_server" "gw_hosts" {
-  # ipfs-ams-gw0
-  name  = "ipfs-${var.region_names[count.index % length(var.regions)]}-gw${(count.index + length(var.regions)) / length(var.regions) - 1}"
+resource "vultr_server" "servers" {
+  name  = "${lookup(var.inventory[count.index], "vultr_name")}"
   tag   = "protocollabs"
-  count = "${var.gw_count * length(var.regions)}"
+  count = "${length(var.inventory)}"
 
-  region_id = "${var.regions[count.index % length(var.regions)]}"
-  plan_id   = 203
+  region_id = "${var.datacenter_to_vultr_region[lookup(var.inventory[count.index], "datacenter")]}"
+  plan_id   = "${lookup(var.inventory[count.index], "vultr_plan")}"
   os_id     = 215
 
-  # gw0.ams.dweblink.net
-  hostname           = "gw${(count.index + length(var.regions)) / length(var.regions) - 1}.${var.region_names[count.index % length(var.regions)]}.dweblink.net"
+  hostname           = "${lookup(var.inventory[count.index], "hostname")}.dweblink.net"
   ipv6               = true
   private_networking = true
   ssh_key_ids        = ["${vultr_ssh_key.lars-desktop.id}"]
 }
 
-resource "vultr_server" "lb_hosts" {
-  # ipfs-ams-lb0
-  name  = "ipfs-${var.region_names[count.index % length(var.regions)]}-lb${(count.index + length(var.regions)) / length(var.regions) - 1}"
-  tag   = "protocollabs"
-  count = "${var.lb_count * length(var.regions)}"
-
-  region_id = "${var.regions[count.index % length(var.regions)]}"
-  plan_id   = 203
-  os_id     = 215
-
-  # lb0.ams.dweblink.net
-  hostname           = "lb${(count.index + length(var.regions)) / length(var.regions) - 1}.${var.region_names[count.index % length(var.regions)]}.dweblink.net"
-  ipv6               = true
-  private_networking = true
-  ssh_key_ids        = ["${vultr_ssh_key.lars-desktop.id}"]
-}
-
-resource "dnsimple_record" "gw_v4hostnames" {
-  count  = "${var.gw_count * length(var.regions)}"
+resource "dnsimple_record" "hostnames_v4" {
+  count  = "${length(var.inventory)}"
   domain = "dweblink.net"
-
-  # gw0.ams
-  name  = "gw${(count.index + length(var.regions)) / length(var.regions) - 1}.${var.region_names[count.index % length(var.regions)]}"
-  value = "${vultr_server.gw_hosts.*.ipv4_address[count.index]}"
+  name  = "${lookup(var.inventory[count.index], "hostname")}"
+  value = "${vultr_server.servers.*.ipv4_address[count.index]}"
   type  = "A"
   ttl   = "60"
 }
 
-resource "dnsimple_record" "gw_v6hostnames" {
-  count  = "${var.gw_count * length(var.regions)}"
+resource "dnsimple_record" "hostnames_v6" {
+  count  = "${length(var.inventory)}"
   domain = "dweblink.net"
-
-  # gw0.ams
-  name  = "gw${(count.index + length(var.regions)) / length(var.regions) - 1}.${var.region_names[count.index % length(var.regions)]}"
-  value = "${vultr_server.gw_hosts.*.ipv6_address[count.index]}"
+  name  = "${lookup(var.inventory[count.index], "hostname")}"
+  value = "${vultr_server.servers.*.ipv6_address[count.index]}"
   type  = "AAAA"
   ttl   = "60"
 }
-
-resource "dnsimple_record" "lb_v4hostnames" {
-  count  = "${var.lb_count * length(var.regions)}"
-  domain = "dweblink.net"
-
-  # lb0.ams
-  name  = "lb${(count.index + length(var.regions)) / length(var.regions) - 1}.${var.region_names[count.index % length(var.regions)]}"
-  value = "${vultr_server.lb_hosts.*.ipv4_address[count.index]}"
-  type  = "A"
-  ttl   = "60"
-}
-
-resource "dnsimple_record" "lb_v6hostnames" {
-  count  = "${var.lb_count * length(var.regions)}"
-  domain = "dweblink.net"
-
-  # lb0.ams
-  name  = "lb${(count.index + length(var.regions)) / length(var.regions) - 1}.${var.region_names[count.index % length(var.regions)]}"
-  value = "${vultr_server.lb_hosts.*.ipv6_address[count.index]}"
-  type  = "AAAA"
-  ttl   = "60"
-}
-
-# resource "random_id" "trigger" {
-#   byte_length = 8
-# }
-
-
-# resource "null_resource" "bootstrap" {
-#   triggers {
-#     trigger = "${random_id.trigger.hex}"
-#   }
-#
-#   provisioner "local-exec" {
-#     command = "echo ${join(", ", vultr_server.gw_hosts.*.ipv4_address)}"
-#   }
-# }
-
 
 # variable "anycast_ip4addrs" {
 #   default = ["198.51.233.233", "198.51.233.234"]
