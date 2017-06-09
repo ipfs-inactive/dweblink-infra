a640ec6b5ecbcd2a0be1d9c00827e73b39edf2a1
Lars Gierth <larsg@systemli.org>
2017-06-09 02:34:52 +0200

    Lay out VPN and more roles, and clean up a bit

--- dweblink.tf
+++ dweblink.tf
@@ -3,17 +3,7 @@ resource "vultr_ssh_key" "lars-desktop" {
   public_key = "${file("lars-desktop.pub")}"
 }
 
-variable "datacenters" {
-  default = ["ams","fra","cdg"]
-}
-variable "regions" {
-  default = {
-    ams = "eu",
-    fra = "eu",
-    cdg = "eu"
-  }
-}
-variable "datacenter_to_vultr_region" {
+variable "dc2region" {
   default = {
     ams = 7,
     fra = 9,
@@ -21,14 +11,28 @@ variable "datacenter_to_vultr_region" {
   }
 }
 variable "inventory" {
-  default = {
-    "0" = { hostname = "gw0.ams", datacenter = "ams", role = "gw", vultr_name = "ipfs-ams-gw0", vultr_plan = 203 },
-    "1" = { hostname = "gw1.ams", datacenter = "ams", role = "gw", vultr_name = "ipfs-ams-gw1", vultr_plan = 203 },
-    "2" = { hostname = "gw0.fra", datacenter = "fra", role = "gw", vultr_name = "ipfs-fra-gw0", vultr_plan = 203 },
-    "3" = { hostname = "gw1.fra", datacenter = "fra", role = "gw", vultr_name = "ipfs-fra-gw1", vultr_plan = 203 },
-    "4" = { hostname = "lb0.ams", datacenter = "ams", role = "lb", vultr_name = "ipfs-ams-lb0", vultr_plan = 203 },
-    "5" = { hostname = "lb0.fra", datacenter = "fra", role = "lb", vultr_name = "ipfs-fra-lb0", vultr_plan = 203 }
-  }
+  default = [
+    { hostname = "vpn0.ams", dc = "ams", vpn_ipv4 = "10.42.1.1", role = "vpn", vultr_name = "ipfs-ams-vpn0", vultr_plan = 201 },
+    { hostname = "coord0.ams", dc = "ams", vpn_ipv4 = "10.42.1.10", role = "coordinator", vultr_name = "ipfs-ams-coord0", vultr_plan = 201 },
+    # { hostname = "mon0.ams", dc = "ams", vpn_ipv4 = "10.42.1.20", role = "monitor", vultr_name = "ipfs-ams-mon0", vultr_plan = 201 },
+    # { hostname = "lb0.ams", dc = "ams", vpn_ipv4 = "10.42.1.100", role = "loadbalancer", vultr_name = "ipfs-ams-lb0", vultr_plan = 201 },
+    # { hostname = "gw0.ams", dc = "ams", vpn_ipv4 = "10.42.1.200", role = "gateway", vultr_name = "ipfs-ams-gw0", vultr_plan = 201 },
+    # { hostname = "gw1.ams", dc = "ams", vpn_ipv4 = "10.42.1.201", role = "gateway", vultr_name = "ipfs-ams-gw1", vultr_plan = 201 },
+
+    { hostname = "vpn0.fra", dc = "fra", vpn_ipv4 = "10.42.2.1", role = "vpn", vultr_name = "ipfs-fra-vpn0", vultr_plan = 201 },
+    { hostname = "coord0.fra", dc = "fra", vpn_ipv4 = "10.42.2.10", role = "coordinator", vultr_name = "ipfs-fra-coord0", vultr_plan = 201 },
+    # { hostname = "mon0.fra", dc = "fra", vpn_ipv4 = "10.42.2.20", role = "monitor", vultr_name = "ipfs-fra-mon0", vultr_plan = 201 },
+    # { hostname = "lb0.fra", dc = "fra", vpn_ipv4 = "10.42.2.100", role = "loadbalancer", vultr_name = "ipfs-fra-lb0", vultr_plan = 201 },
+    # { hostname = "gw0.fra", dc = "fra", vpn_ipv4 = "10.42.2.200", role = "gateway", vultr_name = "ipfs-fra-gw0", vultr_plan = 201 },
+    # { hostname = "gw1.fra", dc = "fra", vpn_ipv4 = "10.42.2.201", role = "gateway", vultr_name = "ipfs-fra-gw1", vultr_plan = 201 },
+
+    { hostname = "vpn0.cdg", dc = "cdg", vpn_ipv4 = "10.42.3.1", role = "vpn", vultr_name = "ipfs-cdg-vpn0", vultr_plan = 201 },
+    { hostname = "coord0.cdg", dc = "cdg", vpn_ipv4 = "10.42.3.10", role = "coordinator", vultr_name = "ipfs-cdg-coord0", vultr_plan = 201 },
+    # { hostname = "mon0.cdg", dc = "cdg", vpn_ipv4 = "10.42.3.20", role = "monitor", vultr_name = "ipfs-cdg-mon0", vultr_plan = 201 },
+    # { hostname = "lb0.cdg", dc = "cdg", vpn_ipv4 = "10.42.3.100", role = "loadbalancer", vultr_name = "ipfs-cdg-lb0", vultr_plan = 201 },
+    # { hostname = "gw0.cdg", dc = "cdg", vpn_ipv4 = "10.42.3.200", role = "gateway", vultr_name = "ipfs-cdg-gw0", vultr_plan = 201 },
+    # { hostname = "gw1.cdg", dc = "cdg", vpn_ipv4 = "10.42.3.201", role = "gateway", vultr_name = "ipfs-cdg-gw1", vultr_plan = 201 },
+  ]
 }
 
 resource "vultr_server" "servers" {
@@ -36,40 +40,21 @@ resource "vultr_server" "servers" {
   tag   = "protocollabs"
   count = "${length(var.inventory)}"
 
-  region_id = "${var.datacenter_to_vultr_region[lookup(var.inventory[count.index], "datacenter")]}"
+  region_id = "${var.dc2region[lookup(var.inventory[count.index], "dc")]}"
   plan_id   = "${lookup(var.inventory[count.index], "vultr_plan")}"
   os_id     = 215
 
   hostname           = "${lookup(var.inventory[count.index], "hostname")}.dweblink.net"
   ipv6               = true
-  private_networking = true
+  private_networking = false
   ssh_key_ids        = ["${vultr_ssh_key.lars-desktop.id}"]
 }
 
-resource "dnsimple_record" "hostnames_v4" {
+resource "dnsimple_record" "hostnames" {
   count  = "${length(var.inventory)}"
   domain = "dweblink.net"
   name  = "${lookup(var.inventory[count.index], "hostname")}"
-  value = "${vultr_server.servers.*.ipv4_address[count.index]}"
+  value = "${lookup(var.inventory[count.index], "vpn_ipv4")}"
   type  = "A"
   ttl   = "60"
 }
-
-resource "dnsimple_record" "hostnames_v6" {
-  count  = "${length(var.inventory)}"
-  domain = "dweblink.net"
-  name  = "${lookup(var.inventory[count.index], "hostname")}"
-  value = "${vultr_server.servers.*.ipv6_address[count.index]}"
-  type  = "AAAA"
-  ttl   = "60"
-}
-
-# variable "anycast_ip4addrs" {
-#   default = ["198.51.233.233", "198.51.233.234"]
-# }
-
-
-# variable "anycast_ip6addrs" {
-#   default = ["2620:2:6000:26::26", "2620:2:6000:26::27"]
-# }
-
