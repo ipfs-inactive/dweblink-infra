9343fb4f5e9abe4b6fa2e491275e5ec20f863df7
Lars Gierth <larsg@systemli.org>
2017-06-23 02:21:10 +0200

    inventory: install a few useful packages

--- base/inventory/main.tf
+++ base/inventory/main.tf
@@ -55,6 +55,22 @@ resource "vultr_server" "hosts" {
   ssh_key_ids        = ["${vultr_ssh_key.hosts.*.id}"]
 }
 
+resource "null_resource" "tools" {
+  count = "${length(var.hosts)}"
+
+  connection {
+    host = "${element(vultr_server.hosts.*.ipv4_address, count.index)}"
+    user = "root"
+    agent = true
+  }
+
+  provisioner "remote-exec" {
+    inline = [
+      "apt-get update && DEBIAN_FRONTEND=noninteractive apt-get install -yq vim screen gdb tree iftop sysstat bridge-utils unzip jq mtr traceroute dnsutils psmisc",
+    ]
+  }
+}
+
 resource "dnsimple_record" "hostnames" {
   count  = "${length(var.hosts)}"
   domain = "${var.domain_name}"
