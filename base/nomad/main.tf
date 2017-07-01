164ee72b730c58b5d7129ce5ebf58c09ab5811d4
Lars Gierth <larsg@systemli.org>
2017-07-01 02:11:45 +0200

    nomad: clean up download artifacts

--- base/nomad/main.tf
+++ base/nomad/main.tf
@@ -68,6 +68,7 @@ resource "null_resource" "install" {
   provisioner "remote-exec" {
     inline = [
       "DEBIAN_FRONTEND=noninteractive apt-get install -yq ethtool",
+      "rm -rf /tmp/nomad*",
       "cd /tmp && wget https://releases.hashicorp.com/nomad/${var.nomad_version}/nomad_${var.nomad_version}_linux_amd64.zip && unzip nomad_${var.nomad_version}_linux_amd64.zip",
       "mv /tmp/nomad /usr/local/bin/",
       "systemctl daemon-reload",
